2a86a366f49be298748e78d32dd3fa7b51a828e6
Dave <Dave7995@gmail.com>
2022-02-10 09:43:54 -0500

    try a commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,9 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
-
-
-
+# Test this out
 
 urlpatterns = [
    url(r'^$',index),  
