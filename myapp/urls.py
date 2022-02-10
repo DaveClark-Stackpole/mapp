9a346433d23805cd5beceee48a3c946146eb7b4b
dave <dave7995@gmail.com>
2022-02-10 09:32:59 -0500

    commit

---
+++ myapp/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from django.views.generic.base import TemplateView
+
+
+
+
+urlpatterns = [
+   url(r'^$',index),  
+]
+
+
+   
