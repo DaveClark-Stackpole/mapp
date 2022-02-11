e0eaf5a45ae82e680b5db312d7fab360f57ee1ef
dave <dave@gmail.com>
2022-02-10 21:01:43 -0500

    update3

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,14 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
-from views_test import one
-
-# Test this out
 
 urlpatterns = [
-   url(r'^$',index),  
-   url(r'^one',one),  
-]
+    path('admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='static_pages/index.html'), name='home'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-   
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
